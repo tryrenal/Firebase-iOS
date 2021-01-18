7f6ac295be191830c10905da0b40c60c6464fd19
renaldy <renalraion@gmail.com>
2021-01-18 09:32:09 +0700

    Initial Commit

---
+++ Firebase iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase iOS
+//
+//  Created by Renaldy on 18/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
